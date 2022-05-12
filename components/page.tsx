f17b62c275aed13921d63eb720a97cf537bd5d00
maystrova <maystrovakate@gmail.com>
2022-05-12 20:09:56 +0300

    icons added

--- components/page.tsx
+++ components/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Head from 'next/head'
 
-interface PageProps {}
-
-const Page = ({}: PageProps) => {
-    return <div></div>
+interface PageProps {
+    children:
+        | React.ReactChildren
+        | React.ReactComponentElement<any>
+        | string
+        | React.ReactNode
+    title?: string
 }
 
-export { Page }
+const Page = ({ children, title }: PageProps) => {
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+                />
+            </Head>
+            {children}
+        </>
+    )
+}
+
+export default Page
