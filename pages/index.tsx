6221a3a4ff65accea0b4aab6ff9454ce95868f86
maystrova <maystrovakate@gmail.com>
2022-05-17 15:49:47 +0300

    styles feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Page from 'components/page'
 import Header from 'components/header'
 
+import style from 'styles/homePage.module.scss'
+
+const title: string = `Let's start exploring`
+
 const HomePage = () => {
     return (
         <Page>
             <Header />
-            <div></div>
+            <div className={style.main}>
+                <h1 className={style.main_title}>
+                    Hey, User! <br /> {title}
+                </h1>
+            </div>
         </Page>
     )
 }
